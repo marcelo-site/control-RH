43952e21d98e7d24dc54b2366d26559a56a8e4d2
Marcelo <57197501+marcelo-site@users.noreply.github.com>
2023-08-17 23:02:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const createNewFile = () => {
         + `-${date.getMonth() < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1}`
         + `-${date.getFullYear()}.txt`
     file.name = arquive,
-        file.path = app.getPath('documents') + '/b' + arquive
+        file.path = app.getPath('documents') + arquive
     return file
 }
 
